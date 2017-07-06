05c43b97f387529b2678552ea8fefabe73fb4b1b
Dustin Van Tate Testa <toast27@gmail.com>
2017-07-06 11:39:37 -0400

    comments

--- led_cart.ino
+++ led_cart.ino
@@ -10,22 +10,27 @@
 #define NUM_LEDS 144
 CRGB leds[NUM_LEDS];
 
+// light and color modes and switching mechanisms
 #include "modes.h"
 
 // check mode change button for input
+// ret - true if mode changed, else false
 bool chkin(){
   // don't want repeated triggering
   static bool changeable = true;
   if (changeable && digitalRead(BUTTON_PIN)) {
+    // if triggered change modes and disable trigger
     changeable = false;
     mode::next();
     return true;
   } else if (!changeable && !digitalRead(BUTTON_PIN)) {
+    // if trigger disabled and they arent pressing btn, re-enable
     changeable = true;
   }
   return false;
 }
 
+// run once at startup
 void setup(){
   // pushbutton to switch modes
   pinMode(BUTTON_PIN, INPUT);
@@ -35,6 +40,7 @@ void setup(){
 
 }
 
+// looped repeatedly forever
 void loop(){
   // check input
   chkin();
